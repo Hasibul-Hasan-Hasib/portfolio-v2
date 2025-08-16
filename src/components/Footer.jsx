d53a95660d874f6ffc60dedc6ac6ea47b92dc465
Hasibul Hasan Hasib <mdhasibulctg100@gmail.com>
2025-08-16 23:55:09 +0600

    feat: add Tabler icons, update Footer component with navigation links, and enhance Layout structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,23 @@
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-            this is footer
-        </div>
-    );
-};
+        <footer className="overflow-hidden mt-auto grid grid-cols-10 items-center text-slate-400 border-t border-slate-500">
+            <span className="px-7 col-span-1">Find me in:</span>
+            <nav className="col-span-9 flex items-center">
+                <NavLink className="px-6 py-4 border-l border-slate-500" to="/">
+                    <IconBrandLinkedin size={24} />
+                </NavLink>
+                <NavLink className="px-6 py-4 border-x border-slate-500" to="/about">
+                    <IconBrandGithub size={24} />
+                </NavLink>
+                <NavLink className="flex ml-auto px-8 py-4 border-l border-slate-500" to="/contact">
+                    @Hasibul-Hasan-Hasib <IconBrandGithub className="ml-2" size={24} />
+                </NavLink>
+            </nav>
+        </footer>
+    )
+}
 
-export default Footer;
+export default Footer
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,9 +5,9 @@ import Footer from '../components/Footer'
 const Layout = () => {
     return (
         <div className='p-8 bg-slate-950'>
-            <div className='rounded-lg bg-slate-800 shadow-lg min-h-[calc(100vh-5rem)] border-1 border-slate-500'>
+            <div className='flex flex-col rounded-lg bg-slate-800 shadow-lg min-h-[calc(100vh-5rem)] border-1 border-slate-500'>
                 <Header />
-                <main>
+                <main className='flex-1'>
                     <Outlet />
                 </main>
                 <Footer />
